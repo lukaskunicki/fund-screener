b0f724e56a467a1b2d724a4d403a21af18c4050a
Lukasz Kunicki <lkunicki@gravityglobal.com>
2022-02-06 20:12:38 +0100

    Open cors for the backend endpoint

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 const responses = require('./responses');
 
 const app = express();
 const port = 3000;
+
+app.use(cors());
 
 app.get('/', (req, res) => {
   const isGrumpy = !!process.env.GRUMPY;
